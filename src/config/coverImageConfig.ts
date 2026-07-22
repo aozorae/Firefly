37808d098ad7fa44bd31e1b0bddd586fb6e5bbde
尘荒三木 <154658461+gdz66601@users.noreply.github.com>
2026-07-22 08:56:32 +0800

    Update src/config/coverImageConfig.ts from Firefly Admin

--- src/config/coverImageConfig.ts
+++ src/config/coverImageConfig.ts
@@ -16,21 +16,15 @@ import type { CoverImageConfig } from "../types/coverImageConfig";
  * ---
  */
 export const coverImageConfig: CoverImageConfig = {
-	// 是否在文章详情页显示封面图
-	enableInPost: true,
-
-	randomCoverImage: {
-		// 随机封面图功能开关
-		enable: false,
-		// 封面图API列表
-		apis: [
+	"enableInPost": true,
+	"randomCoverImage": {
+		"enable": true,
+		"apis": [
 			"https://t.alcy.cc/pc",
 			"https://www.dmoe.cc/random.php",
-			"https://uapis.cn/api/v1/random/image?category=acg&type=pc",
+			"https://uapis.cn/api/v1/random/image?category=acg&type=pc"
 		],
-		// API失败时的回退图片路径（相对于src目录或以/开头的public目录路径）
-		fallback: "assets/images/cover.avif",
-		// 是否显示加载动画
-		showLoading: false,
-	},
+		"fallback": "assets/images/cover.avif",
+		"showLoading": false
+	}
 };
